22716647b5215640428b1c2131b81f8e5c79060f
a0955361101 <0955361101@gmail.com>
2022-06-30 15:51:24 +0800

    Card組件

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -26,32 +26,4 @@ function Card({test}){
     
 }
 
-
-// class Card extends Component {
-
-
-//     render(){
-//         return(
-//             <div className='course_card'>
-//                 <div className='course_card_top'>
-//                     <div className='course_card_level'>初級</div>
-//                 </div>
-//                 <div className='course_card_down'>
-//                     <div className='course_card_txt'>
-//                         <p style={{ 'font-size': 17,'font-weight':'bolder'}}>愛心拉花</p>
-//                         <p  style={{ 'font-size': 10,'color':'#898787',  'line-height': 17}}>課程介紹範例文字與範圍,課程介紹範例文字與範圍,課程介紹範例文字與範圍.</p>
-//                         <div className='d-flex course_card_price'>
-//                             <p style={{ 'font-size': 12,'letter-spacing': '0.07rem'}}>NT$</p>
-//                             <p style={{ 'font-size': 17,'letter-spacing': '0.07rem'}}>2000</p>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-        
-//         )
-//     }
-// }
-
-
-
 export default Card
