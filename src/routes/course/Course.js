a2d0b5fffb7c4d87abfb61ba03e1fa0abc69ad4f
a0955361101 <0955361101@gmail.com>
2022-06-30 16:36:13 +0800

    課程

--- src/routes/course/Course.js
+++ src/routes/course/Course.js
@@ -9,6 +9,7 @@ const Course = () => {
             <NavBar />
             <h2>課程</h2>
             <Card test={{lv:'初級',title:'愛心拉花',txt:'課程介紹範例文字與範圍,課程介紹範例文字與範圍,課程介紹範例文字與範圍.',price:'1000'}} />
+            
         </Fragment>
     )
 
