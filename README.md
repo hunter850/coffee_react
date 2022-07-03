f39db926a5948a557f03d1e2738b5a0100e51f8e
min <a0955361101@gmail.com>
2022-07-03 15:31:16 +0800

    Card

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # 全域CSS (index.css)
 `.title-font - 標題字體-圓體`
 
+### 容器
+
+```
+.container - {
+  width: 1440px;
+  margin: 0 auto;
+  padding: 0 200px;
+}
+```
+
 ### flex 系列
 ```
 .d-flex - display:flex;
 .f-jcc - justify-content: center;
 .f-aic - align-items: center;
+.f-w - flex-wrap: wrap;
 
 ```
 
@@ -16,8 +27,6 @@
 ```
 .card-shadow -  box-shadow: 1px 1px 10px 1px rgba(0, 0, 0, 0.15);
 .br-10 - border-radius: 10px;
-.card-mr - margin-right: 40px;
-.card-mb - margin-bottom: 30px;
 
 
 
--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -13,11 +13,11 @@ function Card({test}){
                 </div>
                 <div className='course_card_down'>
                     <div className='course_card_txt'>
-                        <p style={{ 'fontSize': '1.0625rem','fontWeight':'bolder'}}>{test.title}</p>
+                        <p style={{ 'fontWeight':'bolder'}}>{test.title}</p>
                         <p className='font-min' style={{'color':'#898787'}}>{test.txt}</p>
                         <div className='d-flex course_card_price' >
-                            <p style={{ 'fontSize': 12,'letterSpacing': '0.07rem'}}>NT$</p>
-                            <p style={{ 'fontSize': 17,'letterSpacing': '0.07rem'}}>{test.price}</p>
+                            <p style={{ 'fontSize': '0.75rem','letterSpacing': '0.07rem'}}>NT$</p>
+                            <p style={{ 'fontSize': '1.0625rem','letterSpacing': '0.07rem'}}>{test.price}</p>
                         </div>
                     </div>
                 </div>
