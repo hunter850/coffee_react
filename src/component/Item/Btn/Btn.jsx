42b08494f906e97dd1aedb0cd76b0ba57dd87165
hunterluo <hunter850711@gmail.com>
2022-07-06 22:59:08 +0800

    新增type預設button

--- src/component/Item/Btn/Btn.jsx
+++ src/component/Item/Btn/Btn.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Btn.css'
 
-export default function Btn({ style, children }) {
+export default function Btn({ style, children, type = "button" }) {
     return (
         <div>
             <button
+                type={type}
                 className="coffeeBtn"
                 style={{
                     backgroundColor: style.backgroundColor,
