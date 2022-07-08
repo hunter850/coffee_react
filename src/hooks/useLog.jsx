c09aab2c390f64a781ae7a2b216a165b57707cfb
hunterluo <hunter850711@gmail.com>
2022-07-09 00:04:59 +0800

    更新useLog可傳多參數

--- src/hooks/useLog.jsx
+++ src/hooks/useLog.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react"
 
-const useLog = (value) => {
+const useLog = (...value) => {
     useEffect(() => {
-        console.log(value);
-    }, [value])
+        console.log(...value);
+    }, value);
 }
 
 export default useLog
