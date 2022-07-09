e4a2a6953dfc904f663b06a5e047bbd9b99fe4e7
hunterluo <hunter850711@gmail.com>
2022-07-09 14:29:54 +0800

    add eslint & prettier

--- src/routes/cart/Cart.js
+++ src/routes/cart/Cart.js
@@ -1,15 +1,13 @@
 import { Fragment } from "react";
 import NavBar from "../../component/NavBar";
 
-const Cart = () => {
-    const el = (
+function Cart() {
+    return (
         <Fragment>
             <NavBar />
             <h2>購物車</h2>
         </Fragment>
-    )
-
-    return el;
+    );
 }
 
-export default Cart
+export default Cart;
