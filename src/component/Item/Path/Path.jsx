4149ab31f4e52c70dedb40c8e3f6dd0e8b813374
min <a0955361101@gmail.com>
2022-07-09 23:00:10 +0800

    加入背景色參數

--- src/component/Item/Path/Path.jsx
+++ src/component/Item/Path/Path.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import './Path.css'
-import { v4 } from 'uuid'
+import React from 'react';
+import './Path.css';
+import { v4 } from 'uuid';
 
-export default function Path({ pathObj }) {
+export default function Path ({ pathObj, backgroundColor }) {
     return (
         <div className="container">
-            <div className="Path-font Path-wrap">
+            <div className="Path-font Path-wrap" style={{ backgroundColor: backgroundColor }}>
                 <span>首頁</span>
                 {pathObj.path.map((path) => {
-                    return <span key={v4()}>{path}</span>
+                    return <span key={v4()}>{path}</span>;
                 })}
             </div>
         </div>
-    )
+    );
 }
