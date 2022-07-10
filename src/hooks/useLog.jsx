446c1a847ed86a31d1d6d48283c6b3d318f4754d
hunterluo <hunter850711@gmail.com>
2022-07-10 20:45:15 +0800

    修正useLog dependencies

--- src/hooks/useLog.jsx
+++ src/hooks/useLog.jsx
@@ -3,7 +3,8 @@ import { useEffect } from "react";
 const useLog = (...value) => {
     useEffect(() => {
         console.log(...value);
-    }, [value]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, value);
 };
 
 export default useLog;
