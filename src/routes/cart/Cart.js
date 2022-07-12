f92695b3230d709f801eb56a7cadcf62bd1011a1
hunterluo <hunter850711@gmail.com>
2022-07-13 01:17:01 +0800

    arry提升到cart層級

--- src/routes/cart/Cart.js
+++ src/routes/cart/Cart.js
@@ -1,17 +1,58 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { useStyleChange } from "../../Contexts/SuperProvider";
+import useArray from "../../hooks/useArray";
 import NavBar from "../../component/NavBar";
 import GoodsList from "./components/GoodsList";
 
 function Cart() {
     // useStyleChange();若window.innerWidth <= 375 回傳 1 反之回傳 0
-    const nowStyle = useStyleChange();
+    const breakPoint = useStyleChange();
+    const cartList = useArray([]);
+    useEffect(() => {
+        setTimeout(() => {
+            cartList.cto([
+                {
+                    id: 0,
+                    picture:
+                        "https://cdn.pixabay.com/photo/2019/02/25/04/06/coffee-4018874_960_720.jpg",
+                    name: "藍山豆",
+                    price: 250,
+                    quantity: 2,
+                },
+                {
+                    id: 1,
+                    picture:
+                        "https://cdn.pixabay.com/photo/2019/02/25/04/06/coffee-4018874_960_720.jpg",
+                    name: "哥倫比亞豆",
+                    price: 260,
+                    quantity: 1,
+                },
+                {
+                    id: 2,
+                    picture:
+                        "https://cdn.pixabay.com/photo/2019/02/25/04/06/coffee-4018874_960_720.jpg",
+                    name: "巴西豆",
+                    price: 120,
+                    quantity: 3,
+                },
+                {
+                    id: 3,
+                    picture:
+                        "https://cdn.pixabay.com/photo/2019/02/25/04/06/coffee-4018874_960_720.jpg",
+                    name: "曼特寧豆",
+                    price: 130,
+                    quantity: 1,
+                },
+            ]);
+        }, 50);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <Fragment>
             <NavBar />
-            <h1>{nowStyle}</h1>
-            <GoodsList />
+            <h1>{breakPoint}</h1>
+            <GoodsList cartList={cartList} />
         </Fragment>
     );
 }
--- src/routes/cart/components/GoodsList.jsx
+++ src/routes/cart/components/GoodsList.jsx
@@ -1,13 +1,15 @@
-import { useEffect, useMemo, useCallback } from "react";
+import { useMemo, useCallback } from "react";
 import { useStyleChange } from "../../../Contexts/SuperProvider";
-import useArray from "../../../hooks/useArray";
-import useLog from "../../../hooks/useLog";
+import useDebounce from "../../../hooks/useDebounce";
+// import useLog from "../../../hooks/useLog";
 import CounterGroup from "./CounterGroup";
 
-function GoodsList() {
+function GoodsList({ cartList }) {
     const breakPoint = useStyleChange();
     const styles = useMemo(() => {
-        if (breakPoint === 0) {
+        if (breakPoint === 1) {
+            return {};
+        } else {
             return {
                 listWrap: {
                     width: "71.63%",
@@ -28,58 +30,17 @@ function GoodsList() {
                     objectPosition: "center center",
                 },
             };
-        } else {
-            return {};
         }
     }, [breakPoint]);
-    const cartList = useArray([]);
-    // useDebounce(
-    //     () => {
-    //         console.log(cartList.value);
-    //     },
-    //     300,
-    //     [cartList]
-    // );
-    useLog(cartList);
-    useEffect(() => {
-        setTimeout(() => {
-            cartList.cto([
-                {
-                    id: 0,
-                    picture:
-                        "https://cdn.pixabay.com/photo/2019/02/25/04/06/coffee-4018874_960_720.jpg",
-                    name: "藍山豆",
-                    price: 250,
-                    quantity: 2,
-                },
-                {
-                    id: 1,
-                    picture:
-                        "https://cdn.pixabay.com/photo/2019/02/25/04/06/coffee-4018874_960_720.jpg",
-                    name: "哥倫比亞豆",
-                    price: 260,
-                    quantity: 1,
-                },
-                {
-                    id: 2,
-                    picture:
-                        "https://cdn.pixabay.com/photo/2019/02/25/04/06/coffee-4018874_960_720.jpg",
-                    name: "巴西豆",
-                    price: 120,
-                    quantity: 3,
-                },
-                {
-                    id: 3,
-                    picture:
-                        "https://cdn.pixabay.com/photo/2019/02/25/04/06/coffee-4018874_960_720.jpg",
-                    name: "曼特寧豆",
-                    price: 130,
-                    quantity: 1,
-                },
-            ]);
-        }, 50);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+
+    useDebounce(
+        () => {
+            console.log(cartList.value);
+        },
+        300,
+        [cartList]
+    );
+
     const deleteHandler = useCallback(
         (id) => {
             cartList.cto(cartList.cfilter((item) => item.id !== id));
