0bdfb5935eb70d15cda43ee198e161b5073ad8ac
a0955361101 <0955361101@gmail.com>
2022-07-12 08:51:22 +0800

    Path-RWD字體大小

--- src/component/Item/Path/Path.jsx
+++ src/component/Item/Path/Path.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import './Path.css';
-import { v4 } from 'uuid';
+import React from "react";
+import "./Path.css";
+import { v4 } from "uuid";
 
 export default function Path ({ pathObj, backgroundColor }) {
     return (
         <div className="container">
-            <div className="Path-font Path-wrap" style={{ backgroundColor: backgroundColor }}>
+            <div
+                className="Path-font Path-wrap"
+                style={{ backgroundColor: backgroundColor }}
+            >
                 <span>首頁</span>
                 {pathObj.path.map((path) => {
                     return <span key={v4()}>{path}</span>;
