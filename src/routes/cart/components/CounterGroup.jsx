d2941f7e01918cec94873feee990301b8e630e6e
hunterluo <hunter850711@gmail.com>
2022-07-14 01:52:49 +0800

    調整input判斷 新增css

--- src/routes/cart/components/CounterGroup.jsx
+++ src/routes/cart/components/CounterGroup.jsx
@@ -1,9 +1,49 @@
-import { Fragment, useState, useCallback, useRef } from "react";
+import { Fragment, useState, useCallback, useRef, useMemo } from "react";
+import { useStyleChange } from "../../../Contexts/SuperProvider";
+import plus from "../../../images/cart/plus.svg";
+import minus from "../../../images/cart/minus.svg";
 
 function CounterGroup(props) {
     const { quantity, id, cartList } = props;
+    const breakPoint = useStyleChange();
     const [count, setCount] = useState(quantity);
     const inputRef = useRef(null);
+    const styles = useMemo(() => {
+        if (breakPoint === 1) {
+            return {};
+        } else {
+            return {
+                inputStyle: {
+                    padding: "0px 0px 0px 9px",
+                    height: "36px",
+                    fontSize: "16px",
+                    border: "none",
+                    outline: "none",
+                    borderBottom: "1px solid #324A59",
+                },
+                minusButtonStyle: {
+                    width: "36px",
+                    height: "36px",
+                    backgroundColor: "transparent",
+                    border: "none",
+                    outline: "none",
+                    color: "#324A59",
+                    cursor: "pointer",
+                    background: `url(${minus}) center center / 16px 16px no-repeat`,
+                },
+                plusButtonStyle: {
+                    width: "36px",
+                    height: "36px",
+                    backgroundColor: "transparent",
+                    border: "none",
+                    outline: "none",
+                    color: "#324A59",
+                    cursor: "pointer",
+                    background: `url(${plus}) center center / 16px 16px no-repeat`,
+                },
+            };
+        }
+    }, [breakPoint]);
     const changeHandler = useCallback(
         (event) => {
             if (
@@ -68,32 +108,76 @@ function CounterGroup(props) {
         },
         [count, cartList, id]
     );
-    const selectHandler = useCallback((event) => {
+    const kyeupHandler = useCallback((event) => {
         if (
             Number.isNaN(parseInt(event.currentTarget.value)) ||
-            parseInt(event.currentTarget.value) <= 0
+            parseInt(event.currentTarget.value) <= 0 ||
+            ((event.key === "Backspace" || event.key === "Delete") &&
+                event.currentTarget.value.length === 1)
         ) {
             event.currentTarget.select();
         }
     }, []);
+    const numberKeyDiction = useMemo(() => {
+        return [
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "Backspace",
+            "Delete",
+            "Home",
+            "End",
+            "ArrowLeft",
+            "ArrowRight",
+            "ArrowUp",
+            "ArrowDown",
+            "F5",
+            "Tab",
+        ];
+    }, []);
+    const keydownHandler = useCallback(
+        (event) => {
+            if (
+                numberKeyDiction.indexOf(event.key) === -1 ||
+                event.keyCode === 229
+            ) {
+                event.preventDefault();
+            }
+        },
+        [numberKeyDiction]
+    );
     const clickHandler = useCallback((event) => {
-        if (parseInt(event.target.value) === 0) {
-            event.target.select();
+        if (event.currentTarget.value.length === 1) {
+            event.currentTarget.select();
         }
     }, []);
     return (
         <Fragment>
-            <button onClick={() => countHandler(-1)}>-</button>
+            <button
+                onClick={() => countHandler(-1)}
+                style={styles.minusButtonStyle}
+            ></button>
             <input
                 type="number"
                 value={count}
                 onChange={changeHandler}
                 ref={inputRef}
-                onKeyUp={selectHandler}
+                onKeyUp={kyeupHandler}
+                onKeyDown={keydownHandler}
                 onClick={clickHandler}
+                style={styles.inputStyle}
             />
-            <button onClick={() => countHandler(1)}>+</button>
-            <h1>{count}</h1>
+            <button
+                onClick={() => countHandler(1)}
+                style={styles.plusButtonStyle}
+            ></button>
         </Fragment>
     );
 }
