dd49504e56854743fd91ffcfb10c6b1bc14b282f
iona0511 <lilly800511@gmail.com>
2022-07-13 11:28:42 +0800

    -

---
+++ src/component/Food/components/SlideShow.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./SlideShow.css";
+
+const colors = [
+    "rgb(253, 231, 255)",
+    "rgb(185, 201, 233)",
+    "rgb(227, 231, 190)",
+];
+const delay = 4000;
+
+function Slideshow() {
+    const [index, setIndex] = useState(0);
+    const timeoutRef = useRef(null);
+
+    function resetTimeout() {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+    }
+
+    useEffect(() => {
+        resetTimeout();
+        timeoutRef.current = setTimeout(
+            () =>
+                setIndex((prevIndex) =>
+                    prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+                ),
+            delay
+        );
+
+        return () => {
+            resetTimeout();
+        };
+    }, [index]);
+
+    return (
+        <div className="slideshow">
+            <div
+                className="slideshowSlider"
+                style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+            >
+                {colors.map((backgroundColor, index) => (
+                    <div
+                        className="slide"
+                        key={index}
+                        style={{ backgroundColor }}
+                    ></div>
+                ))}
+            </div>
+
+            <div className="slideshowDots">
+                {colors.map((_, idx) => (
+                    <div
+                        key={idx}
+                        className={`slideshowDot${
+                            index === idx ? " active" : ""
+                        }`}
+                        onClick={() => {
+                            setIndex(idx);
+                        }}
+                    ></div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Slideshow;
