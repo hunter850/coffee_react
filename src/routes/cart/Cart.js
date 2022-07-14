6b6e445e19c94ae627f11fbf14f2f479d36aed31
hunterluo <hunter850711@gmail.com>
2022-07-14 22:22:07 +0800

    add total component

--- src/routes/cart/Cart.js
+++ src/routes/cart/Cart.js
@@ -4,9 +4,11 @@ import useArray from "../../hooks/useArray";
 import NavBar from "../../component/NavBar";
 import GoodsHeader from "./components/GoodsHeader";
 import GoodsList from "./components/GoodsList";
+import TotalHeader from "./components/TotalHeader";
 import TotalBord from "./components/TotalBord";
 import Modal from "../../component/Modal/Modal";
 import ModalContent from "./components/ModalContent";
+import coffee_bean from "../../images/cart/coffee_bean.svg";
 
 function Cart() {
     // useStyleChange();若window.innerWidth <= 375 回傳 1 反之回傳 0
@@ -28,7 +30,7 @@ function Cart() {
                     display: "flex",
                     justifyContent: "space-between",
                     maxWidth: "1440px",
-                    margin: "0px auto",
+                    margin: "48px auto 0px",
                     backgroundColor: "var(--CREAM)",
                     outline: "1px solid red",
                 },
@@ -46,32 +48,28 @@ function Cart() {
             cartList.cto([
                 {
                     id: 0,
-                    picture:
-                        "https://cdn.pixabay.com/photo/2019/02/25/04/06/coffee-4018874_960_720.jpg",
+                    picture: coffee_bean,
                     name: "藍山豆",
                     price: 250,
                     quantity: 2,
                 },
                 {
                     id: 1,
-                    picture:
-                        "https://cdn.pixabay.com/photo/2019/02/25/04/06/coffee-4018874_960_720.jpg",
+                    picture: coffee_bean,
                     name: "哥倫比亞豆",
                     price: 260,
                     quantity: 1,
                 },
                 {
                     id: 2,
-                    picture:
-                        "https://cdn.pixabay.com/photo/2019/02/25/04/06/coffee-4018874_960_720.jpg",
+                    picture: coffee_bean,
                     name: "巴西豆",
                     price: 120,
                     quantity: 3,
                 },
                 {
                     id: 3,
-                    picture:
-                        "https://cdn.pixabay.com/photo/2019/02/25/04/06/coffee-4018874_960_720.jpg",
+                    picture: coffee_bean,
                     name: "曼特寧豆",
                     price: 130,
                     quantity: 1,
@@ -86,7 +84,7 @@ function Cart() {
             <NavBar />
             <div style={styles.cartContainer}>
                 <div style={styles.listWrap}>
-                    <GoodsHeader />
+                    {cartList.clength() >= 1 && <GoodsHeader />}
                     <GoodsList
                         cartList={cartList}
                         setDeleteId={setDeleteId}
@@ -94,6 +92,7 @@ function Cart() {
                     />
                 </div>
                 <div style={styles.totalWrap}>
+                    <TotalHeader />
                     <TotalBord />
                 </div>
             </div>
