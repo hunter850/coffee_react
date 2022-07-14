9e44c5e2f9e7092ba4eb320a6ec9315779a40735
a0955361101 <0955361101@gmail.com>
2022-07-14 08:52:23 +0800

    css

--- src/component/Course/Header/Header.jsx
+++ src/component/Course/Header/Header.jsx
@@ -1,39 +1,90 @@
 import React from "react";
-import './Header.css'
+import "./Header.css";
 
-
-export default function Header(){
-    return(
+export default function Header () {
+    return (
         <div className="Header">
             <div>
-                <div className="Header-title"><p >帶你深入探索有趣又獨特的咖啡體驗課程</p></div>
+                <div className="Header-title">
+                    <p>帶你深入探索有趣又獨特的咖啡體驗課程</p>
+                </div>
                 <div className="Header-input">
                     <div className="search-wrap">
-                        <input type="text" placeholder="搜尋想要的課程"/>
+                        <input type="text" placeholder="搜尋想要的課程" />
                         <div className="search-btn">
-                            <svg width="32" height="33" viewBox="0 0 32 33" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <g filter="url(#filter0_d_1908_2814)">
-                            <circle cx="13.375" cy="13.375" r="8.375" stroke="white" strokeWidth="2"/>
-                            <line x1="20.4142" y1="20.5" x2="25" y2="25.0858" stroke="white" strokeWidth="2" strokeLinecap="round"/>
-                            </g>
-                            <defs>
-                            <filter id="filter0_d_1908_2814" x="0" y="0" width="32" height="32.0859" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                            <feFlood floodOpacity="0" result="BackgroundImageFix"/>
-                            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-                            <feOffset dx="1" dy="1"/>
-                            <feGaussianBlur stdDeviation="2.5"/>
-                            <feComposite in2="hardAlpha" operator="out"/>
-                            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-                            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1908_2814"/>
-                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1908_2814" result="shape"/>
-                            </filter>
-                            </defs>
+                            <svg
+                                width="32"
+                                height="33"
+                                viewBox="0 0 32 33"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <g filter="url(#filter0_d_1908_2814)">
+                                    <circle
+                                        cx="13.375"
+                                        cy="13.375"
+                                        r="8.375"
+                                        stroke="white"
+                                        strokeWidth="2"
+                                    />
+                                    <line
+                                        x1="20.4142"
+                                        y1="20.5"
+                                        x2="25"
+                                        y2="25.0858"
+                                        stroke="white"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                    />
+                                </g>
+                                <defs>
+                                    <filter
+                                        id="filter0_d_1908_2814"
+                                        x="0"
+                                        y="0"
+                                        width="32"
+                                        height="32.0859"
+                                        filterUnits="userSpaceOnUse"
+                                        colorInterpolationFilters="sRGB"
+                                    >
+                                        <feFlood
+                                            floodOpacity="0"
+                                            result="BackgroundImageFix"
+                                        />
+                                        <feColorMatrix
+                                            in="SourceAlpha"
+                                            type="matrix"
+                                            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                                            result="hardAlpha"
+                                        />
+                                        <feOffset dx="1" dy="1" />
+                                        <feGaussianBlur stdDeviation="2.5" />
+                                        <feComposite
+                                            in2="hardAlpha"
+                                            operator="out"
+                                        />
+                                        <feColorMatrix
+                                            type="matrix"
+                                            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+                                        />
+                                        <feBlend
+                                            mode="normal"
+                                            in2="BackgroundImageFix"
+                                            result="effect1_dropShadow_1908_2814"
+                                        />
+                                        <feBlend
+                                            mode="normal"
+                                            in="SourceGraphic"
+                                            in2="effect1_dropShadow_1908_2814"
+                                            result="shape"
+                                        />
+                                    </filter>
+                                </defs>
                             </svg>
-                            </div>
+                        </div>
                     </div>
                 </div>
-                
             </div>
         </div>
-    )
+    );
 }
