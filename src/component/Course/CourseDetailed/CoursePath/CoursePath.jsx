c7d88559b0803e1ebf36f8b918de26ac065c509a
min <a0955361101@gmail.com>
2022-07-15 21:34:14 +0800

    麵包屑高亮

--- src/component/Course/CourseDetailed/CoursePath/CoursePath.jsx
+++ src/component/Course/CourseDetailed/CoursePath/CoursePath.jsx
@@ -1,35 +1,54 @@
 import "./CoursePath.css";
 
+import { useState } from "react";
+const initState = (items) => {
+    const menuItems = [];
+    for (let i = 0; i < items.length; i++) {
+        menuItems.push({
+            id: i + 1,
+            name: items[i],
+            focus: false,
+        });
+    }
+
+    return menuItems;
+};
+
 function CoursePath() {
+    const coursePaths = [
+        "課程內容",
+        "適合對象",
+        "需求材料",
+        "報名資訊",
+        "注意事項",
+    ];
+    const defaultCoursePaths = initState(coursePaths);
+    const [clickSure, setClickSure] = useState(defaultCoursePaths);
+
     return (
         <div className="CoursePath">
-            <div
-                className="CoursePath-content focus"
-                style={{ paddingBottom: "23px" }}
-            >
-                課程內容
-            </div>
-            <div
-                className="CoursePath-object"
-                style={{ paddingBottom: "23px" }}
-            >
-                適合對象
-            </div>
-            <div
-                className="CoursePath-material"
-                style={{ paddingBottom: "23px" }}
-            >
-                需求材料
-            </div>
-            <div className="CoursePath-news" style={{ paddingBottom: "23px" }}>
-                報名資訊
-            </div>
-            <div
-                className="CoursePath-matter"
-                style={{ paddingBottom: "23px" }}
-            >
-                注意事項
-            </div>
+            {clickSure.map((v, i) => {
+                console.log(v);
+                return (
+                    <div
+                        key={i}
+                        className={`CoursePaths ${v.focus ? "focus" : ""}`}
+                        onClick={() => {
+                            const newCoursePaths = defaultCoursePaths.map(
+                                (v, index) => {
+                                    if (i === index)
+                                        return { ...v, focus: true };
+
+                                    return v;
+                                }
+                            );
+                            setClickSure(newCoursePaths);
+                        }}
+                    >
+                        {v.name}
+                    </div>
+                );
+            })}
         </div>
     );
 }
