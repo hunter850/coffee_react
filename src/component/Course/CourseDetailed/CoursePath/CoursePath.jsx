8fceaaf6b3761379cd07f2cc64da3543f15b868c
min <a0955361101@gmail.com>
2022-07-16 00:07:43 +0800

    課程路徑高亮2.0

--- src/component/Course/CourseDetailed/CoursePath/CoursePath.jsx
+++ src/component/Course/CourseDetailed/CoursePath/CoursePath.jsx
@@ -1,20 +1,12 @@
 import "./CoursePath.css";
-
-import { useState } from "react";
-const initState = (items) => {
-    const menuItems = [];
-    for (let i = 0; i < items.length; i++) {
-        menuItems.push({
-            id: i + 1,
-            name: items[i],
-            focus: false,
-        });
-    }
-
-    return menuItems;
-};
+import { useState, useEffect } from "react";
 
 function CoursePath() {
+    // 初次渲染預設高亮顯示
+    useEffect(() => {
+        setStart(0);
+    }, []);
+
     const coursePaths = [
         "課程內容",
         "適合對象",
@@ -22,23 +14,36 @@ function CoursePath() {
         "報名資訊",
         "注意事項",
     ];
+    // 初始高亮的狀態
+    const [start, setStart] = useState(1);
+    // 生成物件型狀態的函式
+    const initState = (items) => {
+        const menuItems = [];
+        for (let i = 0; i < items.length; i++) {
+            menuItems.push({
+                id: i + 1,
+                name: items[i],
+                focus: start === 1 && i === 0 ? true : false,
+            });
+        }
+        return menuItems;
+    };
+
     const defaultCoursePaths = initState(coursePaths);
     const [clickSure, setClickSure] = useState(defaultCoursePaths);
-
     return (
         <div className="CoursePath">
             {clickSure.map((v, i) => {
-                console.log(v);
                 return (
                     <div
                         key={i}
-                        className={`CoursePaths ${v.focus ? "focus" : ""}`}
+                        className={`CoursePaths ${v.focus ? "focus" : ""} `}
                         onClick={() => {
                             const newCoursePaths = defaultCoursePaths.map(
                                 (v, index) => {
-                                    if (i === index)
+                                    if (i === index) {
                                         return { ...v, focus: true };
-
+                                    }
                                     return v;
                                 }
                             );
