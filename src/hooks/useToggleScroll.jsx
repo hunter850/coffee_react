3b8100f0b3b7fff9fde9a1ebcb8a452254461c5a
hunterluo <hunter850711@gmail.com>
2022-07-17 22:53:53 +0800

    useToggleScroll write in useCallback

--- src/hooks/useToggleScroll.jsx
+++ src/hooks/useToggleScroll.jsx
@@ -1,11 +1,13 @@
+import { useCallback } from "react";
+
 const useToggleScroll = () => {
-    return function () {
+    return useCallback(() => {
         if (getComputedStyle(document.body).overflow !== "hidden") {
             document.body.style.overflow = "hidden";
         } else {
             document.body.style.overflow = "visible";
         }
-    };
+    }, []);
 };
 
 export default useToggleScroll;
