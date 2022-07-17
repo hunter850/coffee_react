f145a06d0833f98098f2378a939e53bb3771a85f
min <a0955361101@gmail.com>
2022-07-17 20:04:35 +0800

    排版修正

--- src/component/Course/CourseManage/List/List.jsx
+++ src/component/Course/CourseManage/List/List.jsx
@@ -1,18 +1,20 @@
-import './List.css';
+import "./List.css";
 
-function List () {
+function List() {
     return (
-        <div className='courseList d-flex f-aic'>
-            <div className='List-img'><img src="" alt="" /></div>
-            <div className='List-title'>愛心拉花</div>
-            <div className='List-level'>初級</div>
-            <div className='List-price'>NT$ &nbsp;<span style={{ fontSize: '1.0625rem' }}>2000</span></div>
-            <div className='List-edit-icon'></div>
-            <div className='List-delete-icon'></div>
+        <div className="courseList d-flex f-aic">
+            <div className="List-img">
+                <img src="" alt="" />
+            </div>
+            <div className="List-title">愛心拉花</div>
+            <div className="List-level">初級</div>
+            <div className="List-price">
+                NT$ &nbsp;<span style={{ fontSize: "1.0625rem" }}>2000</span>
+            </div>
+            <div className="List-edit-icon"></div>
+            <div className="List-delete-icon"></div>
         </div>
     );
 }
-
-
 
 export default List;
