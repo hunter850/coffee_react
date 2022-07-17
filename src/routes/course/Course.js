28ec027942dc3ccd4a3765f591c0b51543ee5036
min <a0955361101@gmail.com>
2022-07-17 21:56:19 +0800

    排序功能-(價格.難度)

--- src/routes/course/Course.js
+++ src/routes/course/Course.js
@@ -8,12 +8,90 @@ import Sort from "../../component/Course/Sort/Sort";
 import axios from "axios";
 
 const Course = () => {
+    const [sortData, setSortData] = useState("");
     // Header搜尋框的狀態 - 狀態提升放這邊
     const [searchInp, setSearchInp] = useState("");
     // 渲染卡片內容用的狀態
     const [courseData, setCourseData] = useState([]);
     // 搜尋框值清空時渲染用的狀態
     const [courseDataCopy, setCourseDataCopy] = useState([]);
+
+    // console.log(courseData);
+
+    const priceAsc = (a, b) => {
+        return a.course_price - b.course_price;
+    };
+    const priceDesc = (a, b) => {
+        return b.course_price - a.course_price;
+    };
+    const levelAsc = (a, b) => {
+        return a.course_level - b.course_level;
+    };
+    const levelDesc = (a, b) => {
+        return b.course_level - a.course_level;
+    };
+
+    // 排序 - 價錢低到高
+    if (sortData === "priceAsc") {
+        courseData.sort(priceAsc);
+        for (let i = 0; i < courseData.length; i++) {
+            if (Number(courseData[i].course_level) === 1) {
+                courseData[i].course_level = "初級";
+            }
+            if (Number(courseData[i].course_level) === 2) {
+                courseData[i].course_level = "中級";
+            }
+            if (Number(courseData[i].course_level) === 3) {
+                courseData[i].course_level = "高級";
+            }
+        }
+    }
+    // 排序 - 價錢高到低
+    if (sortData === "priceDesc") {
+        courseData.sort(priceDesc);
+        for (let i = 0; i < courseData.length; i++) {
+            if (Number(courseData[i].course_level) === 1) {
+                courseData[i].course_level = "初級";
+            }
+            if (Number(courseData[i].course_level) === 2) {
+                courseData[i].course_level = "中級";
+            }
+            if (Number(courseData[i].course_level) === 3) {
+                courseData[i].course_level = "高級";
+            }
+        }
+    }
+
+    // 排序 - 難度初級到高級
+    if (sortData === "levelAsc") {
+        courseData.sort(levelAsc);
+        for (let i = 0; i < courseData.length; i++) {
+            if (Number(courseData[i].course_level) === 1) {
+                courseData[i].course_level = "初級";
+            }
+            if (Number(courseData[i].course_level) === 2) {
+                courseData[i].course_level = "中級";
+            }
+            if (Number(courseData[i].course_level) === 3) {
+                courseData[i].course_level = "高級";
+            }
+        }
+    }
+    // 排序 - 難度高級到初級
+    if (sortData === "levelDesc") {
+        courseData.sort(levelDesc);
+        for (let i = 0; i < courseData.length; i++) {
+            if (Number(courseData[i].course_level) === 1) {
+                courseData[i].course_level = "初級";
+            }
+            if (Number(courseData[i].course_level) === 2) {
+                courseData[i].course_level = "中級";
+            }
+            if (Number(courseData[i].course_level) === 3) {
+                courseData[i].course_level = "高級";
+            }
+        }
+    }
     // 搜尋框為空值時重新渲染
     useEffect(() => {
         if (searchInp === "") {
@@ -25,7 +103,17 @@ const Course = () => {
         axios
             .get("http://localhost:3600/coffee")
             .then((res) => {
-                // console.log(res.data);
+                // 深拷貝一組沒有把數字轉換成中文的資料
+                // 難度排序時使用
+                const newSortData = JSON.parse(JSON.stringify(res.data));
+                // console.log(sortData);
+                if (sortData === "levelAsc") {
+                    return setCourseData(newSortData);
+                }
+                if (sortData === "levelDesc") {
+                    return setCourseData(newSortData);
+                }
+
                 for (let i = 0; i < res.data.length; i++) {
                     if (Number(res.data[i].course_level) === 1) {
                         res.data[i].course_level = "初級";
@@ -37,6 +125,7 @@ const Course = () => {
                         res.data[i].course_level = "高級";
                     }
                 }
+
                 const newCourseData = res.data;
                 setCourseData(newCourseData);
                 setCourseDataCopy(newCourseData);
@@ -44,8 +133,7 @@ const Course = () => {
             .catch((err) => {
                 console.log(err.response);
             });
-    }, []);
-    // console.log(courseData);
+    }, [sortData]);
 
     const el = (
         <Fragment>
@@ -58,7 +146,11 @@ const Course = () => {
                     setSearchInp={setSearchInp}
                     searchInp={searchInp}
                 />
-                <Sort courseData={courseData} />
+                <Sort
+                    courseData={courseData}
+                    sortData={sortData}
+                    setSortData={setSortData}
+                />
                 <div className="container">
                     <div className="d-flex f-w card-wrap">
                         <Link to="/course/detailed/001">
