afe469c8e993c7f45cf4a71db3a269f9396eb533
hunterluo <hunter850711@gmail.com>
2022-07-23 00:27:32 +0800

    fake data

---
+++ src/routes/cart/fakeData/productData.js
@@ -0,0 +1,53 @@
+import coffee_bean from "../../../images/cart/coffee_bean.svg";
+
+export const productData = [
+    {
+        id: 0,
+        picture: coffee_bean,
+        name: "藍山豆",
+        price: 250,
+        quantity: 2,
+    },
+    {
+        id: 1,
+        picture: coffee_bean,
+        name: "哥倫比亞豆",
+        price: 260,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        picture: coffee_bean,
+        name: "巴西豆",
+        price: 120,
+        quantity: 3,
+    },
+    {
+        id: 3,
+        picture: coffee_bean,
+        name: "曼特寧豆",
+        price: 130,
+        quantity: 1,
+    },
+    {
+        id: 4,
+        picture: coffee_bean,
+        name: "淺培咖啡豆",
+        price: 130,
+        quantity: 1,
+    },
+    {
+        id: 5,
+        picture: coffee_bean,
+        name: "中培咖啡豆",
+        price: 130,
+        quantity: 1,
+    },
+    {
+        id: 6,
+        picture: coffee_bean,
+        name: "深培咖啡豆",
+        price: 130,
+        quantity: 1,
+    },
+];
