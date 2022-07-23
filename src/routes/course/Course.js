31dcc610e4c4a528a3a302cadf6fac7effb412c5
min <a0955361101@gmail.com>
2022-07-23 20:54:19 +0800

    分頁 + 搜尋 + 排序 整合

--- src/routes/course/Course.js
+++ src/routes/course/Course.js
@@ -1,3 +1,5 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Fragment, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
@@ -38,6 +40,8 @@ const numberConvertString = (data) => {
     }
 };
 
+// chunk - 依size分成子陣列，ex. chunk([1, 2, 3, 4, 5], 2) -> [[1,2],[3,4],[5]]
+// https://stackoverflow.com/questions/8495687/split-array-into-chunks
 const chunk = (arr, size) =>
     Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
         arr.slice(i * size, i * size + size)
@@ -48,52 +52,35 @@ const Course = () => {
     const [sortData, setSortData] = useState("");
     // Header搜尋框的狀態 - 狀態提升放這邊
     const [searchInp, setSearchInp] = useState("");
-    // 渲染卡片內容用的狀態
+    // 判斷是否點擊搜尋按鈕
+    const [searchSure, setSearchSure] = useState(false);
+    // 主要渲染畫面的資料
     const [courseData, setCourseData] = useState([]);
-    // 搜尋框值清空時渲染用的狀態
+    // 備份的資料,提供給難度排序(因為資料庫來的是數字,所以需要使用深拷貝出來還未被轉成中文的)跟搜尋框清空時使用
     const [courseDataCopy, setCourseDataCopy] = useState([]);
-
+    // 備份的資料,提供給價格排序跟一般搜尋使用
+    const [dataDisplay, setDataDisplay] = useState([]);
     //預設第一頁
     const [pageNow, setPageNow] = useState(1);
     //預設一頁幾筆
-    const [perPage, setPerPage] = useState(5);
-    // 等伺服器抓完資料才知道多少(didMount時決定)
+    const [perPage, setPerPage] = useState(8);
+    // 總頁數,等伺服器抓完資料才知道多少(didMount時決定)
     const [pageTotal, setPageTotal] = useState(0);
-    const [dataDisplay, setDataDisplay] = useState([]);
-    // console.log(courseData);
-    // console.log(courseData);
-    // console.log(courseDataCopy);
-
-    // 搜尋框為空值時重置原始資料
-    useEffect(() => {
-        if (searchInp === "") {
-            setCourseData(courseData);
-        }
-    }, [searchInp, courseData]);
 
     const getCourseData = async () =>
         await axios
             .get(courseDataGet)
             .then((res) => {
-                // 深拷貝一組沒有把數字轉換成中文的資料
-                // 難度排序時使用
+                // 深拷貝一組沒有把數字轉換成中文的資料, 難度排序時使用
                 const newSortData = JSON.parse(JSON.stringify(res.data));
-                // console.log(sortData);
 
-                // if (sortData === "levelAsc") {
-                //     return setCourseDataCopy(newSortData);
-                // }
-                // if (sortData === "levelDesc") {
-                //     return setCourseDataCopy(newSortData);
-                // }
-                // 將資料庫的course_level數字轉換成中文
-                // numberConvertString(res.data);
                 const newCourseData = res.data;
+                // 將資料庫的course_level數字轉換成中文
                 numberConvertString(newCourseData);
-                // setCourseData(newCourseData);
-
+                // 將資料分別存進兩個狀態
                 setDataDisplay(newCourseData);
                 setCourseDataCopy(newSortData);
+                // 首次渲染的資料操作
                 const pageArray = chunk(newCourseData, perPage);
                 if (pageArray.length > 0) {
                     setPageTotal(pageArray.length);
@@ -149,9 +136,36 @@ const Course = () => {
             .catch((err) => {
                 console.log(err.response);
             });
+
+    // 向資料庫發請求,獲取資料
     useEffect(() => {
         getCourseData();
-    }, [sortData, pageNow]);
+    }, [sortData]);
+
+    // 搜尋框為空值時重置原始資料
+    useEffect(() => {
+        if (searchInp === "") {
+            numberConvertString(courseDataCopy);
+            const pageArray = chunk(courseDataCopy, perPage);
+            if (pageArray.length > 0) {
+                setPageTotal(pageArray.length);
+                setCourseData(pageArray);
+            }
+        }
+    }, [searchInp]);
+
+    // 一般搜尋框搜尋的渲染
+    useEffect(() => {
+        if (searchSure === true) {
+            setPageNow(1);
+            const pageArray = chunk(dataDisplay, perPage);
+            if (pageArray.length > 0) {
+                setPageTotal(pageArray.length);
+                setCourseData(pageArray);
+            }
+            setSearchSure(false);
+        }
+    }, [searchSure]);
 
     const el = (
         <Fragment>
@@ -159,10 +173,11 @@ const Course = () => {
                 <NavBar />
                 <Path pathObj={{ path: ["．課程資訊"] }} />
                 <Header
-                    courseData={courseData}
-                    setCourseData={setCourseData}
                     searchInp={searchInp}
                     setSearchInp={setSearchInp}
+                    dataDisplay={dataDisplay}
+                    setDataDisplay={setDataDisplay}
+                    setSearchSure={setSearchSure}
                 />
                 <Sort
                     courseData={courseData}
@@ -206,7 +221,7 @@ const Course = () => {
                                 );
                             })}
                     </div>
-                    <div className="d-flex">
+                    <div className="d-flex f-jcc">
                         {Array(pageTotal)
                             .fill(1)
                             .map((v, i) => {
@@ -216,6 +231,10 @@ const Course = () => {
                                         onClick={() => {
                                             setPageNow(i + 1);
                                         }}
+                                        className={`course-page-btn ${pageNow === i + 1
+                                            ? "course-page-btn-focus"
+                                            : ""
+                                            }`}
                                     >
                                         {i + 1}
                                     </div>
