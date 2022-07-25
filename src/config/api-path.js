c75794784cf39be6ab9ec222567e016d01d60977
Tommy <sky003428@gmail.com>
2022-07-25 16:17:38 +0800

    0725

--- src/config/api-path.js
+++ src/config/api-path.js
@@ -1,4 +1,12 @@
 export const SERVER = "http://localhost:3500";
+export const imgSrc = `${SERVER}/images`;
+
+// COURSE
 export const courseDataGet = `${SERVER}/coffee-course-get`;
 export const courseDataFkGet = `${SERVER}/coffee-courseFK-get`;
+
+// FOOD
 export const foodDataGet = `${SERVER}/coffee-food-get`;
+
+// SHARING
+export const getPosts = `${SERVER}/sharing/api`;
--- src/routes/sharing/Sharing.js
+++ src/routes/sharing/Sharing.js
@@ -1,11 +1,44 @@
-import { Fragment } from "react";
+import { wrap } from "lodash";
+import axios from "axios";
+import { Fragment, useEffect, useState } from "react";
+
+import { getPosts, imgSrc } from "./../../config/api-path";
 import NavBar from "../../component/NavBar";
+import styles from "./css/sharing.module.scss";
 
 function Sharing() {
+    const { sharing_wrap } = styles;
+    const [rows, setRows] = useState([]);
+
+    const getData = async () => {
+        const r = await axios(getPosts);
+
+        setRows(r.data.rows);
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
     return (
         <Fragment>
             <NavBar />
             <h2>分享牆</h2>
+            <h5>{JSON.stringify(rows[0])}</h5>
+            <div className={sharing_wrap} style={{ display: "flex" }}>
+                {rows.map((v, i) => {
+                    return (
+                        <div key={i}>
+                            <img
+                                src={`${imgSrc}/${v.src}`}
+                                alt={`pic${v.sid}`}
+                                className="post_img"
+                            />
+                            <p>{v.title}</p>
+                        </div>
+                    );
+                })}
+            </div>
         </Fragment>
     );
 }
