4a5416f8a7f2de2aa84b93187f4b30d26f290266
Rowan Zeng <zpxoci0619@gmail.com>
2022-07-28 00:41:20 +0800

    0728 12:40

--- src/component/FrontPages/LatestNews/LatestNews.jsx
+++ src/component/FrontPages/LatestNews/LatestNews.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React,{ Fragment } from "react";
+// import { Link } from "react-router-dom";
+// import LatestTitle from "./LatestTitle";
 
 function LatestNews() {
     return (
-        <>
-            <div className="bottom-line m-auto">
-                <h2 className="home-title">最新消息</h2>
+        <Fragment>
+            <div className="bag-color">
+                <div className="bottom-line m-auto">
+                    <h2 className="home-title">最新消息</h2>
+                </div>
+                <div className="d-flex">
+                    {/* <LatestTitle /> */}
+                </div>
             </div>
-        </>
+        </Fragment>
     );
 }
 
