5a0dcbe7527e647f2566de3b811bf3304972d411
min <a0955361101@gmail.com>
2022-07-27 23:02:07 +0800

    新增 api

--- src/config/api-path.js
+++ src/config/api-path.js
@@ -5,6 +5,7 @@ export const imgSrc = `${SERVER}/images`;
 
 export const courseDataGet = `${SERVER}/course`;
 export const courseDataFkGet = `${SERVER}/course/FK-get`;
+export const courseDataAdd = `${SERVER}/course/add`;
 
 // FOOD
 export const foodDataGet = `${SERVER}/food`;
