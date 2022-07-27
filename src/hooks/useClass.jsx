39ac728316c17398a690c9d39ad73f9f03055763
hunter850 <91997043+hunter850@users.noreply.github.com>
2022-07-27 10:03:08 +0800

    update useClass

--- src/hooks/useClass.jsx
+++ src/hooks/useClass.jsx
@@ -1,8 +1,32 @@
 import { useCallback } from "react";
+// import { cloneDeep } from "lodash";
 
 function useClass() {
     return useCallback((...ar) => {
-        return ar.join(" ");
+        // const tempArray = cloneDeep(ar);
+        const outputArray = [];
+        ar.forEach((item) => {
+            if (typeof item === "string") {
+                outputArray.push(item);
+                return;
+            }
+            if (Array.isArray(item)) {
+                item.forEach((str) => {
+                    if (typeof str === "string") outputArray.push(str);
+                });
+                return;
+            }
+            if (typeof item === "object") {
+                for (let i in item) {
+                    if (!!item[i] === true) {
+                        outputArray.push(i);
+                    }
+                }
+                return;
+            }
+            return;
+        });
+        return outputArray.join(" ");
     }, []);
 }
 
