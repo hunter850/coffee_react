e0b66bae9b072cf4ce2a4ad0474512d22ec6a301
a0955361101 <0955361101@gmail.com>
2022-07-28 09:04:42 +0800

    沒放圖片 先註解掉

--- src/component/FrontPages/HotFoods/HotfoodCard.js
+++ src/component/FrontPages/HotFoods/HotfoodCard.js
@@ -1,16 +1,16 @@
 import React, { Fragment } from "react";
-import HotFoodImg from "../../../images/frontpage/food/Rectangle 133.png";
+// import HotFoodImg from "../../../images/frontpage/food/Rectangle 133.png";
 import "./HotfoodCard.css";
 
-export default function HotfoodCard({hotfoodmargin}) {
+export default function HotfoodCard({ hotfoodmargin }) {
     return (
         <Fragment>
             <div
                 className="hotfood-container m-auto hotfood-circle"
-                style={{ marginTop : hotfoodmargin }}
+                style={{ marginTop: hotfoodmargin }}
             >
                 <div>
-                    <img src={HotFoodImg} alt="" width="230px" height="230px" />
+                    {/* <img src={HotFoodImg} alt="" width="230px" height="230px" /> */}
                 </div>
                 <p className="hotfood-name">凱薩沙拉</p>
                 <p>NT$ 499</p>
