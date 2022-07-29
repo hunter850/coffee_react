c068f5035aea87cf8a6ded183f1db04fc023a34d
hunterluo <hunter850711@gmail.com>
2022-07-29 22:41:08 +0800

    get coupon fetch

--- src/Contexts/initData.js
+++ src/Contexts/initData.js
@@ -2,6 +2,8 @@ export const initData = {
     nowList: "productList",
     productList: [],
     foodList: [],
+    productCoupons: [],
+    foodCoupons: [],
     coupons: [],
     selectedCoupon: -1,
     total: 0,
