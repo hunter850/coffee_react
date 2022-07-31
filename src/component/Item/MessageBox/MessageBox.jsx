843c5e3075ae07f6000a3555d27e52e43ce740f0
min <a0955361101@gmail.com>
2022-08-01 02:00:25 +0800

    警示框更改架構

--- src/component/Item/MessageBox/MessageBox.jsx
+++ src/component/Item/MessageBox/MessageBox.jsx
@@ -2,14 +2,16 @@ import "./MessageBox.scss";
 function MessageBox() {
     return (
         <>
-            <div className="MessageBox">
-                <div className="MessageBox-title">警告視窗</div>
-                <div className="MessageBox-txt">
-                    確定要刪除xxx嗎?真的確定嗎?要不要再考慮一下?真的要這樣?
-                </div>
-                <div className="d-flex MessageBox-btn-wrap">
-                    <div className="MessageBox-confirm-btn">確認</div>
-                    <div className="MessageBox-cancel-btn">取消</div>
+            <div className="MessageBox-body">
+                <div className="MessageBox">
+                    <div className="MessageBox-title">警告視窗</div>
+                    <div className="MessageBox-txt">
+                        確定要刪除xxx嗎?真的確定嗎?要不要再考慮一下?真的要這樣?
+                    </div>
+                    <div className="d-flex MessageBox-btn-wrap">
+                        <div className="MessageBox-confirm-btn">確認</div>
+                        <div className="MessageBox-cancel-btn">取消</div>
+                    </div>
                 </div>
             </div>
         </>
