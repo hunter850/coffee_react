42cc5bdbe84ad64c238492de199c47174710d6c4
RowanZeng <zpxoci0619@gmail.com>
2022-08-02 18:11:40 +0800

    0802 1811

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -9,79 +9,81 @@ import { Link } from "react-router-dom";
 export default function Footer({ bagcolorblue }) {
     return (
         <Fragment>
-            <div
-                className="home-container"
-                style={{ backgroundColor: bagcolorblue }}
-            >
-                <div className="d-flex partition">
-                    <div className="footerlogo-wrap">
-                        <img
-                            src={coffeeLogo}
-                            width="80px"
-                            height="80px"
-                            alt=""
-                        />
+            <home-box>
+                <div
+                    className="home-container"
+                    style={{ backgroundColor: bagcolorblue }}
+                >
+                    <div className="d-flex partition">
+                        <div className="footerlogo-wrap">
+                            <img
+                                src={coffeeLogo}
+                                width="80px"
+                                height="80px"
+                                alt=""
+                            />
+                        </div>
+                        <div className="footertext-wrap">
+                            <ul className="d-flex footertext">
+                                <Link to="/">
+                                    <li className="footer-pdr-48 linkcolor-white">店家資訊</li>
+                                </Link>
+                                <Link to="/">
+                                    <li className="footer-pdr-82 linkcolor-white">關於我們</li>
+                                </Link>
+                                <Link to="/">
+                                    <li className="footer-pdr-48 linkcolor-white">最新活動</li>
+                                </Link>
+                            </ul>
+                            <ul className="d-flex footertext">
+                                <Link to="/">
+                                    <li className="footer-pdr-48 linkcolor-white">夥伴招募</li>
+                                </Link>
+                                <Link to="/">
+                                    <li className="footer-pdr-48 linkcolor-white">會員權益聲明</li>
+                                </Link>
+                                <Link to="/">
+                                    <li className="footer-pdr-48 linkcolor-white">企業服務</li>
+                                </Link>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="footertext-wrap">
-                        <ul className="d-flex footertext">
+                    <div className="sociallink-wrap">
+                        <p className="Copyrighttext">
+                            © 2022 Copyright 0+B Coffee Shop All Rights Reserved.
+                        </p>
+                        <div className="sociallink">
                             <Link to="/">
-                                <li className="footer-pdr-48 linkcolor-white">店家資訊</li>
+                                <img
+                                    className="sociallink-icon"
+                                    src={ttlogo}
+                                    width="48px"
+                                    height="48px"
+                                    alt=""
+                                />
                             </Link>
                             <Link to="/">
-                                <li className="footer-pdr-82 linkcolor-white">關於我們</li>
+                                <img
+                                    className="sociallink-icon"
+                                    src={fblogo}
+                                    width="48px"
+                                    height="48px"
+                                    alt=""
+                                />
                             </Link>
                             <Link to="/">
-                                <li className="footer-pdr-48 linkcolor-white">最新活動</li>
+                                <img
+                                    className="sociallink-icon"
+                                    src={iglogo}
+                                    width="48px"
+                                    height="48px"
+                                    alt=""
+                                />
                             </Link>
-                        </ul>
-                        <ul className="d-flex footertext">
-                            <Link to="/">
-                                <li className="footer-pdr-48 linkcolor-white">夥伴招募</li>
-                            </Link>
-                            <Link to="/">
-                                <li className="footer-pdr-48 linkcolor-white">會員權益聲明</li>
-                            </Link>
-                            <Link to="/">
-                                <li className="footer-pdr-48 linkcolor-white">企業服務</li>
-                            </Link>
-                        </ul>
+                        </div>
                     </div>
                 </div>
-                <div className="sociallink-wrap">
-                    <p className="Copyrighttext">
-                        © 2022 Copyright 0+B Coffee Shop All Rights Reserved.
-                    </p>
-                    <div className="sociallink">
-                        <Link to="/">
-                            <img
-                                className="sociallink-icon"
-                                src={ttlogo}
-                                width="48px"
-                                height="48px"
-                                alt=""
-                            />
-                        </Link>
-                        <Link to="/">
-                            <img
-                                className="sociallink-icon"
-                                src={fblogo}
-                                width="48px"
-                                height="48px"
-                                alt=""
-                            />
-                        </Link>
-                        <Link to="/">
-                            <img
-                                className="sociallink-icon"
-                                src={iglogo}
-                                width="48px"
-                                height="48px"
-                                alt=""
-                            />
-                        </Link>
-                    </div>
-                </div>
-            </div>
+            </home-box>
         </Fragment>
     );
 }
