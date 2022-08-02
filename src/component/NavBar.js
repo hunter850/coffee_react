6c97ef5192004e77db1854ef4d8c692d0871beee
a0955361101 <0955361101@gmail.com>
2022-08-02 14:37:54 +0800

    新增課程進入後台的路由

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -7,7 +7,7 @@ import AuthContext from "./Member/AuthContext";
 const NavBar = () => {
     // 判斷是否為管理者用
     const { sid } = useContext(AuthContext);
-    console.log(sid);
+    // console.log(sid);
     // =============================================
     const memberListyle = {
         position: "relative",
@@ -165,7 +165,14 @@ const NavBar = () => {
                             </Link>
                         </li>
                         <li>
-                            <Link to="/course" style={{ color: "white" }}>
+                            <Link
+                                to={
+                                    Number(sid) === 43
+                                        ? "/course/manage"
+                                        : `/course`
+                                }
+                                style={{ color: "white" }}
+                            >
                                 課程
                             </Link>
                         </li>
