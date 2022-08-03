56c29a4933e84b88915d5d1c38c5e8af0bc74e66
hunterluo <hunter850711@gmail.com>
2022-08-03 21:33:48 +0800

    rename path

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef, useContext } from "react";
 import { Link } from "react-router-dom";
-import "./NavBar.css";
+import "./navBar.css";
 import coffeeLogo from "../../src/images/frontpage/0+B(白).png";
 
 import memberbtnImg from "../../src/images/frontpage/member-icon.png";
