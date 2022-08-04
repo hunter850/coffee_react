61ba07c8d7bce3d53a4319a6c7c258e5cbc204db
TommyLin <sky003428@gmail.com>
2022-08-05 02:48:49 +0800

    css,login判斷顯示

--- src/routes/sharing/components/PostDetailModal/Comment.js
+++ src/routes/sharing/components/PostDetailModal/Comment.js
@@ -1,13 +1,36 @@
 import React from "react";
+import { useAuth } from "../../../../component/Member/AuthContextProvider";
+import useTimeAbout from "../../../../hooks/useTimeAbout";
 
 import { imgSrc } from "../../../../config/api-path";
 import styles from "../../css/Comment.module.scss";
 import Reply from "./Reply";
 
 function Comment({ data }) {
-    const { member_sid, nickname, avatar, content, replies, reply } = data;
+    const { authorized, sid, account, token } = useAuth();
+    const {
+        member_sid,
+        nickname,
+        avatar,
+        content,
+        replies,
+        reply,
+        created_at,
+    } = data;
 
-    const { wrap, comment_wrap, img_wrap, name_wrap, content_wrap } = styles;
+    const timeAbout = useTimeAbout();
+
+    const {
+        wrap,
+        comment_wrap,
+        img_wrap,
+        name_wrap,
+        content_wrap,
+        class_nickname,
+        grey_span,
+        time_wrap,
+    } = styles;
+
     return (
         <div className={wrap}>
             <div className={comment_wrap}>
@@ -15,12 +38,21 @@ function Comment({ data }) {
                     <img src={`${imgSrc}/member/${avatar}`} alt="avatar" />
                 </div>
                 <div className={name_wrap}>
-                    <p style={{ whiteSpace: "nowrap" }}>{nickname}</p>
-                    <span>{member_sid}</span>
+                    <span className={class_nickname}>{nickname}</span>
                 </div>
                 <div className={content_wrap}>
                     <span>{content}</span>
                 </div>
+            </div>
+            <div className={time_wrap}>
+                <span className={`${grey_span} me-3`}>
+                    {timeAbout(created_at)}
+                </span>
+                {member_sid === sid ? (
+                    <span className={grey_span}>刪除</span>
+                ) : (
+                    <span className={grey_span}>回覆</span>
+                )}
             </div>
             {reply && reply.map((v, i) => <Reply data={v} />)}
         </div>
