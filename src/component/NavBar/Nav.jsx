3e6471c642ac3129ae0bf0663cd7f863c0d2b11f
a0955361101 <0955361101@gmail.com>
2022-08-04 13:29:28 +0800

    nav context

---
+++ src/component/NavBar/Nav.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./NavBar";
+import NavWrap from "./NavWrap";
+
+function Nav() {
+    return (
+        <NavWrap>
+            <div>
+                <NavBar />
+            </div>
+        </NavWrap>
+    );
+}
+
+export default Nav;
--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -4,29 +4,31 @@
 import "./NavBar.scss";
 import { Link } from "react-router-dom";
 import Logo from "./Logo/Logo";
-import React, { useState, useEffect, useContext, useCallback } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useAuth, authOrigin } from "../Member/AuthContextProvider";
 import CartCount from "../../Contexts/CartCount";
-import axios, { Axios } from "axios";
-import { getCartCount } from "../../config/api-path";
+// import axios, { Axios } from "axios";
+// import { getCartCount } from "../../config/api-path";
+import { useGetCart } from "./NavWrap";
 
 export const CountContext = React.createContext();
 
 
 function NavBar({ navPosition = 'fixed' }) {
     const { sid, name, setAuth, token } = useAuth();
+    const { getCount, count } = useGetCart();
 
-    const [count, setCount] = useState(0);
+    // const [count, setCount] = useState(0);
 
-    const getCount = useCallback(() => {
-        axios.get(getCartCount, {
-            headers: { Authorization: `Bearer ${token}` },
-        })
-            .then((res) => {
-                setCount(res.data.cartTotalCount);
-                // console.log(res.data.cartTotalCount);
-            });
-    }, []);
+    // const getCount = useCallback(() => {
+    //     axios.get(getCartCount, {
+    //         headers: { Authorization: `Bearer ${token}` },
+    //     })
+    //         .then((res) => {
+    //             setCount(res.data.cartTotalCount);
+    //             // console.log(res.data.cartTotalCount);
+    //         });
+    // }, []);
 
 
     // console.log(cartCount);
@@ -74,6 +76,9 @@ function NavBar({ navPosition = 'fixed' }) {
             }
         });
     }, [windowsWidth, mediaS]);
+    useEffect(() => {
+        getCount();
+    }, []);
 
     // 刪除 auth - 登入狀態
     const handleSignOut = () => {
@@ -122,7 +127,6 @@ function NavBar({ navPosition = 'fixed' }) {
 
     return (
         <CountContext.Provider value={getCount}>
-            <button onClick={getCount}>click</button>
             <header className="nav-header" style={{ position: navPosition }}>
                 <nav className="container  nav-header-wrap" >
                     <div className="nav-menu">
