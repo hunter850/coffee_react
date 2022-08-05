55a0e4b689cb95d605fc7bf69a5c04f3919388a8
Rowan Zeng <zpxoci0619@gmail.com>
2022-08-05 15:38:09 +0800

    0805 1537

--- src/routes/frontPage/LatestnewsDetail.js
+++ src/routes/frontPage/LatestnewsDetail.js
@@ -2,12 +2,15 @@ import React, { Fragment } from "react";
 import leastnewdetailImg1 from "../../images/frontpage/latestnews/news-img1.png";
 import "./LatestnewsDetail.css";
 import Tag from "../../component/Item/Tag/Tag";
+import linedeco from "../../images/products/line_deco.png";
 
 export default function LatestnewsDetail() {
     return (
         <Fragment>
             <div className="detail-container">
-                <h2>50萬會員慶 週週咖啡金再送黑咖啡</h2>
+                <h2 className="leastnewtitle">
+                    50萬會員慶 週週咖啡金再送黑咖啡
+                </h2>
                 <div className="latestnews-tag d-flex">
                     <Tag
                         tagContext="優惠"
@@ -26,9 +29,17 @@ export default function LatestnewsDetail() {
                         alt=""
                     />
                 </div>
-                <p className="smallleastnewtitle">
-                    門市限定咖啡豆採購方案 最低3包即享優惠 滿$200還享外送服務
+                <p className="latestnewdetailcontent1">
+                    當與家有更長的相處，就需要讓更多新鮮事為生活加分<br/>來拎+B
+                    全台門市提供新鮮烘焙咖啡豆採購方案！ 最低3包就能享有多包優惠<br/>
+                    還有寄豆服務，每次取貨都新鮮滿分😋<br/>Home café
+                    就選 來拎+B ☕️ <br/>大宗採購優惠方案<br/> ♦3~5包 95折 <br/> ♦6~9包 93折<br/>
+                    ♦10~20包 9折<br/> ♦21包以上88折<br/>咖啡日常好好享受，精品現折15元，
+                    為讓您的日常沖煮新模樣，多點講究更多點享受，即日起開始優惠!!
                 </p>
+                <div className="newslinedeco">
+                    <img src={linedeco} alt="" />
+                </div>
                 <p>
                     cama café 歡慶50萬會員 推出週週50元咖啡金和寄杯寄豆超值方案
                     新會員再送黑咖啡 最懂咖啡的行家都在cama café！
@@ -38,9 +49,6 @@ export default function LatestnewsDetail() {
                     為了感謝每位咖啡迷的陪伴，即日起展開為期30天的會員慶，推出誠意滿滿的會員優惠方案，
                     活動期間加入的前五萬名新會員，除了同享優惠，還可以免費獲得一張經典黑咖啡兌換券
                 </p>
-                <div>
-                    <img src="" alt="" />
-                </div>
                 <div>
                     <p></p>
                 </div>
