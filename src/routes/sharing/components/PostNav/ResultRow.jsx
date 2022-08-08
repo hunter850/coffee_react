3edd95cd4ccd091e9637c6c040ce059ecaf1b7ae
TommyLin <sky003428@gmail.com>
2022-08-09 04:47:00 +0800

    preview pop tag

--- src/routes/sharing/components/PostNav/ResultRow.jsx
+++ src/routes/sharing/components/PostNav/ResultRow.jsx
@@ -6,7 +6,7 @@ import Magnifier from "./Magnifier";
 import styles from "../../css/Seachbar.module.scss";
 
 function ResultRow({ data }) {
-    const { name, type, m_sid, src } = data;
+    const { name, type, save_at, member_sid, post_sid, author, src } = data;
     const {
         magnifier_wrap,
         rect_wrap,
@@ -18,14 +18,18 @@ function ResultRow({ data }) {
 
     const iconEl = (type) => {
         if (type === "tag") {
-            return <BsTagFill style={{ height: "1.4rem" }} />;
+            return save_at === "tag" ? (
+                <Magnifier />
+            ) : (
+                <BsTagFill style={{ height: "1.4rem" }} />
+            );
         } else if (type === "title") {
             return (
                 <div className={rect_wrap}>
                     <img src={`${sharingIMGS}/${src}`} alt="post" />
                 </div>
             );
-        } else if (type === "member") {
+        } else if (type === "nickname") {
             return (
                 <div className={img_wrap}>
                     <img
@@ -39,11 +43,25 @@ function ResultRow({ data }) {
     };
 
     const nameEl = (type) => {
-        if (type === "member") {
+        if (type === "nickname" || type === "title") {
             return (
                 <div className={member_wrap}>
                     <span style={{ lineHeight: "1.2rem" }}>{name}</span>
-                    <span className={member_small}>#{m_sid}</span>
+                    <span className={member_small}>
+                        {type === "nickname" ? `#${member_sid}` : author}
+                    </span>
+                </div>
+            );
+        } else if (type === "tag" && save_at === "tag") {
+            return (
+                <div className="d-flex w100 align-items-center">
+                    <span className={result_name}>{name}</span>
+                    <span
+                        className={`${member_small} ms-auto me-2`}
+                        style={{ verticalAlign: "bottom" }}
+                    >
+                        熱門標籤
+                    </span>
                 </div>
             );
         }
