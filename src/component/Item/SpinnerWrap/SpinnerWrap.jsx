c47a9ffde3c50f54ce0894ac752732f12f335965
hunter850 <91997043+hunter850@users.noreply.github.com>
2022-08-16 15:37:47 +0800

    spinner

--- src/component/Item/SpinnerWrap/SpinnerWrap.jsx
+++ src/component/Item/SpinnerWrap/SpinnerWrap.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
+import NavBar from "../../NavBar/NavBar";
 import ElementWrap from "./ElementWrap";
 
 function SpinnerWrap(props) {
-    const { children, spinner = "", component = null } = props;
+    const { children, component = null, time = 2000 } = props;
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, time);
+    }, [time]);
     return (
         <ElementWrap component={component}>
-            {props.in === true || props.in === undefined ? children : spinner}
+            {loading ? (
+                <>
+                    <NavBar />
+                    <div className="storeLoadingBox">
+                        <div className="StoreLoading">
+                            <div className="StoreLoadingWrap00">
+                                <img
+                                    src={"/spinner/0Bicon.png"}
+                                    alt=""
+                                    className="animateStore popStore"
+                                    style={{ animationDuration: `${time}ms` }}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </>
+            ) : (
+                children
+            )}
         </ElementWrap>
     );
 }
