06ee5f9cf9df0d32aa3d724e135be47e054f6c63
Tommy <sky003428@gmail.com>
2022-08-16 18:45:36 +0800

    tag select&cancel,css fix

--- src/routes/sharing/components/Tag.jsx
+++ src/routes/sharing/components/Tag.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 
-function Tag({children}) {
+function Tag({ className = "", children }) {
     const styleObj = {
-        marginRight: ".5rem",
+        // marginRight: ".5rem",
         backgroundColor: "#C8D2DC",
         color: "#253945",
         borderRadius: "1rem",
         fontSize: "14px",
         padding: "2px 10px",
         marginBottom: "6px",
+        whiteSpace: "nowrap",
     };
 
-    return <span style={styleObj} className="animate__animated">{children}</span>;
+    return (
+        <span style={styleObj} className={`animate__animated ${className}`}>
+            {children}
+        </span>
+    );
 }
 
 export default Tag;
