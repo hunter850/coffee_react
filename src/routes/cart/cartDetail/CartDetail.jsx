ff6b4c6350e4c3c35955790c93290eec3c312bb4
hunterluo <hunter850711@gmail.com>
2022-08-18 00:56:51 +0800

    detail fetch data & css adjust

--- src/routes/cart/cartDetail/CartDetail.jsx
+++ src/routes/cart/cartDetail/CartDetail.jsx
@@ -1,4 +1,5 @@
-import { Fragment, useEffect } from "react";
+import { Fragment, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import useClass from "../../../hooks/useClass";
 import useData from "../../../hooks/useData";
 import { useAuth } from "../../../component/Member/AuthContextProvider";
@@ -13,21 +14,53 @@ import { cartDetail } from "../../../config/api-path";
 
 function CartDetail() {
     const { container, px_200 } = bs_flex;
-    const { fake_body, order_number_label, order_number } = styles;
+    const {
+        fake_body,
+        order_number_label,
+        order_number,
+        header_text,
+        list,
+        list_name,
+        list_quantity,
+    } = styles;
+    const navigate = useNavigate();
     const c = useClass();
     const [lastInsertId, , resetlastInsertId] = useData("lastInsertId");
     const { token } = useAuth();
+    const [detail, setDetail] = useState({
+        orderNumber: -1,
+        price: 0,
+    });
+    const [detailList, setDetailList] = useState([]);
     useEffect(() => {
-        axios.get(cartDetail, {
-            params: {
-                insertId: lastInsertId,
-            },
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
+        if (lastInsertId === -1) {
+            alert("尚未購物");
+            navigate("/", { replace: true });
+        }
+        axios
+            .get(cartDetail, {
+                params: {
+                    insertId: lastInsertId,
+                },
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            .then((result) => {
+                console.log(result.data);
+                setDetail(() => ({
+                    orderNumber: result.data.orderNumber,
+                    price: result.data.price,
+                }));
+                setDetailList(result.data.rawData);
+            });
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+    useEffect(() => {
+        return () => {
+            resetlastInsertId();
+        };
+    }, [resetlastInsertId]);
     return (
         <Fragment>
             <div className={fake_body}>
@@ -36,15 +69,32 @@ function CartDetail() {
                     <OrderDetails>
                         <OrderDetails.Wrap>
                             <OrderDetails.Header>
-                                <h6>您購買的商品為</h6>
+                                <h6 className={header_text}>您購買的商品為</h6>
                             </OrderDetails.Header>
                             <OrderDetails.Body>
-                                <h1>Body</h1>
+                                <ul>
+                                    <li className={list}>
+                                        <p className={list_name}>商品</p>
+                                        <p className={list_quantity}>數量</p>
+                                    </li>
+                                </ul>
+                                <ul>
+                                    {detailList.map((item) => (
+                                        <li key={item.id} className={list}>
+                                            <p className={list_name}>
+                                                {item.name}
+                                            </p>
+                                            <p className={list_quantity}>
+                                                {item.quantity}
+                                            </p>
+                                        </li>
+                                    ))}
+                                </ul>
                             </OrderDetails.Body>
                         </OrderDetails.Wrap>
                         <OrderDetails.Footer>
                             <p className={order_number_label}>訂單編號</p>
-                            <p className={order_number}>123456789</p>
+                            <p className={order_number}>{detail.orderNumber}</p>
                         </OrderDetails.Footer>
                     </OrderDetails>
                 </div>
