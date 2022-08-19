17fbd81faf54b32ccff7192f972e30964ce1d076
minchia0616 <chia87616@gmail.com>
2022-08-19 14:57:43 +0800

    增加星星

--- src/routes/member/Member.js
+++ src/routes/member/Member.js
@@ -259,6 +259,11 @@ function Member() {
                             <img src={cupStar} alt="" className="cup-star-f"/>
                             <img src={cupStar} alt="" className="cup-star-g"/>
                             <img src={cupStar} alt="" className="cup-star-h"/>
+                            <img src={cupStar} alt="" className="cup-star-i"/>
+                            <img src={cupStar} alt="" className="cup-star-j"/>
+                            <img src={cupStar} alt="" className="cup-star-k"/>
+                            <img src={cupStar} alt="" className="cup-star-l"/>
+                            <img src={cupStar} alt="" className="cup-star-m"/>
                         </div>
                         <div className="mc-level-wrap">
                             <p className="mc-level-title">{
