1dfbb6d9e140260d7446a2e9373c959490dabc8e
Rowan Zeng <zpxoci0619@gmail.com>
2022-08-20 21:55:35 +0800

    0820 2200

--- src/routes/frontPage/FrontPage.js
+++ src/routes/frontPage/FrontPage.js
@@ -16,8 +16,8 @@ import Spinnerwrap from "../../component/Item/SpinnerWrap/SpinnerWrap";
 import useData from "../../hooks/useData";
 
 function FrontPage() {
-    const [frontpagecoupon,setFrontpagecoupon] = useData("frontpagecoupon");
-    
+    const [frontpagecoupon, setFrontpagecoupon] = useData("frontpagecoupon");
+
     // const [isOpen, setIsOpen] = useState(false);
     // useEffect(() => {
     //     if (frontpagecoupon) {
@@ -48,13 +48,13 @@ function FrontPage() {
             <Modal isOpen={frontpagecoupon} setIsOpen={setFrontpagecoupon}>
                 <Modal.Body component="div" style={{ padding: "0px" }}>
                     <div>
-                        <Link to="/getcoupon">
-                            <img
-                                style={{ width: "588px" }}
-                                src={frontpagecouponimg}
-                                alt=""
-                            />
-                        </Link>
+                        {/* <Link to="/getcoupon"> */}
+                        <img
+                            style={{ width: "588px" }}
+                            src={frontpagecouponimg}
+                            alt=""
+                        />
+                        {/* </Link> */}
                     </div>
                 </Modal.Body>
             </Modal>
