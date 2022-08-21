9b43168d900074b8a1add80f42c413a526eccf22
TommyLin <sky003428@gmail.com>
2022-08-22 02:49:17 +0800

    bug fix

--- src/routes/sharing/components/PostDetailModal/Likes.jsx
+++ src/routes/sharing/components/PostDetailModal/Likes.jsx
@@ -1,16 +1,18 @@
 import styles from "./scss/Likes.module.scss";
 
 function Likes({ likes, memberLikePost, didLiked }) {
-    const { like_wrap, heart, liked } = styles;
+    const { like_wrap, container, heart, liked } = styles;
 
     return (
         <div className={like_wrap} onClick={memberLikePost}>
-            <div className={`${heart} ${didLiked ? liked : ""}`}></div>
+            <div className={container}>
+                <div className={`${heart} ${didLiked ? liked : ""}`}></div>
+            </div>
             <span
                 style={{
                     fontSize: "15px",
                     color: "#787878",
-                    marginLeft: "1.7rem",
+                    marginLeft: "1.75rem",
                     marginRight: ".25rem",
                 }}
             >
